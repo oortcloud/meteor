a134ff8bdb020066528904f267a58dacc93b4d2d
Tom Coleman <tom@thesnail.org>
2014-02-17 11:30:09 +1100

    for testing

--- packages/meteor/startup_server.js
+++ packages/meteor/startup_server.js
@@ -1,4 +1,4 @@
 Meteor.startup = function (callback) {
   __meteor_bootstrap__.startup_hooks.push(callback);
-  console.log("Meteor is instrumented for meteorite test (tag/mrt-test-tag)");
+  console.log("Meteor is instrumented for meteorite test (branch/mrt-branch-test)");
 };
